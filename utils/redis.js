c6a9d62aa18055aa636f09cc805334ecd98b3d2d
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2023-04-08 00:54:24 +0100

    adjusted brackets

--- utils/redis.js
+++ utils/redis.js
@@ -6,6 +6,7 @@ class RedisClient {
 
     this.client.on('error', (error) => {
       console.error('Redis client error:', error);
+      throw error; // Propagate the error
     });
   }
 
@@ -17,6 +18,7 @@ class RedisClient {
     return new Promise((resolve, reject) => {
       this.client.get(key, (error, value) => {
         if (error) {
+	  console.error('Redis get error:', error); // Log the error
 	  reject(error);
 	} else {
 	  resolve(value);
@@ -29,6 +31,7 @@ class RedisClient {
     return new Promise((resolve, reject) => {
       this.client.set(key, value, 'EX', duration, (error, result) => {
         if (error) {
+	  console.error('Redis ser error:', error); // Log the error
 	  reject(error);
 	} else {
 	  resolve(result);
@@ -41,10 +44,19 @@ class RedisClient {
     return new Promise((resolve, reject) => {
       this.client.del(key, (error, result) => {
         if (error) {
+	  console.error('Redis del error:', error); // Log the error
           reject(error);
 	} else {
 	  resolve(result);
 	}
+      });
+    });
+  }
+
+  async close() {
+    return new Promise(resolve) => {
+      this.client.quit(() => {
+        resolve();
       });
     });
   }
