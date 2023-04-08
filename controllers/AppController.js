7da0f15ea8baeeeb2efef998f621fdb0b43bc7ad
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2023-04-08 02:22:44 +0100

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,1 +1,19 @@
-controllers.js
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+// return if Redis is alive and if the DB is alive
+class AppController {
+  static getStatus(request, response) {
+    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+  }
+
+  // return the number of users and files in DB:
+  // { "users": 12, "files": 1231 } with a status code 200
+  static async getStats(request, response) {
+    const usersNum = await dbClient.nbUsers();
+    const filesNum = await dbClient.nbFiles();
+    response.status(200).json({ users: usersNum, files: filesNum });
+  }
+}
+
+module.exports = AppController;
