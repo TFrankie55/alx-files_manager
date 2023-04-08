2da67328970e910f104c8c380f8c4f3735ccb5a9
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2023-04-08 02:25:14 +0100

    contains the definitions of two endpoints

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,13 +1,13 @@
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
-// return if Redis is alive and if the DB is alive
+// returns if Redis is alive and if the DB is alive
 class AppController {
   static getStatus(request, response) {
-    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+    response.status(200).json({ "redis": true, "db": true });
   }
 
-  // return the number of users and files in DB:
+  // returns the number of users and files in DB:
   // { "users": 12, "files": 1231 } with a status code 200
   static async getStats(request, response) {
     const usersNum = await dbClient.nbUsers();
