119abd1ee4f870b5f331c34b875a2f183b4d15b4
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2023-04-08 02:15:37 +0100

    contains all endpoints of our API

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,34 @@
-routes
+import { Router } from 'express';
+
+import FilesController from '../controllers/FilesController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import AppController from '../controllers/AppController';
+
+const router = Router();
+
+router.get('/status', AppController.getStatus);
+
+router.get('/stats', AppController.getStats);
+
+router.post('/users', UsersController.postNew);
+
+router.get('/disconnect', AuthController.getDisconnect);
+
+router.get('/connect', AuthController.getConnect);
+
+router.get('/users/me', UsersController.getMe);
+
+router.post('/files', FilesController.postUpload);
+
+router.get('/files', FilesController.getIndex);
+
+router.get('/files/:id', FilesController.getShow);
+
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+router.put('/files/:id/publish', FilesController.putPublish);
+
+router.get('/files/:id/data', FilesController.getFile);
+
+module.exports = router;
